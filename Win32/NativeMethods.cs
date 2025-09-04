8d4bac0a0a6f322921a7851fa04be49930a13ae1
Float Web <67027034+floatweb@users.noreply.github.com>
2025-09-05 01:53:45 +0300

    Add files via upload

---
+++ Win32/NativeMethods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace Win11_Extra_Clock.Win32
+{
+    internal static class NativeMethods
+    {
+        // WinEvent hook
+        public const uint EVENT_OBJECT_CREATE = 0x8000;
+        public const uint EVENT_OBJECT_DESTROY = 0x8001;
+        public const uint EVENT_OBJECT_SHOW = 0x8002;
+        public const uint EVENT_OBJECT_HIDE = 0x8003;
+        public const uint WINEVENT_OUTOFCONTEXT = 0x0000;
+
+
+        public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+
+        public static string GetClassName(IntPtr hwnd)
+        {
+            var sb = new StringBuilder(256);
+            if (GetClassName(hwnd, sb, sb.Capacity) != 0)
+                return sb.ToString();
+            return string.Empty;
+        }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+
+        public static string GetWindowText(IntPtr hwnd)
+        {
+            var sb = new StringBuilder(512);
+            if (GetWindowText(hwnd, sb, sb.Capacity) > 0) return sb.ToString();
+            return string.Empty;
+        }
+    }
+}
